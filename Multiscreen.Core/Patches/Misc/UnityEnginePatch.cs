6cb6a1dd167d8047b0dcc708259da06952b036f4
AMacro <andrew@andrewcraigmackenzie.com>
2025-02-28 12:33:31 +1000

    Fix for Unity 2022.3.58f1 bug
    Unity have refactored `MultipleDisplayUtilities.RelativeMouseAtScaled()` and in the process, have broken it.
    This commit adds a patch to bypass the changes.

---
+++ Multiscreen.Core/Patches/Misc/UnityEnginePatch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Logger = Multiscreen.Util.Logger;
+
+namespace Multiscreen.Patches.Misc;
+#if BETA_VERSION
+
+[HarmonyPatch(typeof(MultipleDisplayUtilities))]
+public class MultipleDisplayUtilitiesPatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(MultipleDisplayUtilities), nameof(MultipleDisplayUtilities.RelativeMouseAtScaled))]
+    private static bool RelativeMouseAtScaled(ref Vector3 __result, Vector2 position, ref int displayIndex)
+    {
+        var result = Display.RelativeMouseAt(position);
+
+        if (result.z == 0)
+        return true;
+
+        __result = result;
+        return false;
+
+    }
+}
+#endif
