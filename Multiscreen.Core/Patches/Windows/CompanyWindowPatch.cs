1fd51afd043d604d06c2f0121b082bd0069da71f
AMacro <andrew@andrewcraigmackenzie.com>
2025-03-23 12:41:51 +1000

    CompanyWindowPatch superseded by generic patch

--- Multiscreen.Core/Patches/Windows/CompanyWindowPatch.cs
+++ Multiscreen.Core/Patches/Windows/CompanyWindowPatch.cs
@@ -5,17 +5,17 @@ using UI.CompanyWindow;
 
 namespace Multiscreen.Patches.Windows;
 
-[HarmonyPatch(typeof(CompanyWindow))]
+//[HarmonyPatch(typeof(CompanyWindow))]
 
-public static class CompanyWindowPatch
-{
+//public static class CompanyWindowPatch
+//{
 
-    [HarmonyPrefix]
-    [HarmonyPatch(typeof(CompanyWindow), nameof(CompanyWindow.Awake))]
-    private static bool Awake(CompanyWindow __instance)
-    {
-        Logger.LogTrace($"CompanyWindow.Awake() {__instance.name}");
-        __instance.SetDisplay(1); //todo update this to use settings
-        return true;
-    }
-}
+//    [HarmonyPrefix]
+//    [HarmonyPatch(typeof(CompanyWindow), nameof(CompanyWindow.Awake))]
+//    private static bool Awake(CompanyWindow __instance)
+//    {
+//        Logger.LogTrace($"CompanyWindow.Awake() {__instance.name}");
+//        __instance.SetDisplay(1); //todo update this to use settings
+//        return true;
+//    }
+//}
